fe07337debeadf108dbb227eba0c6f71ae15ab19
ArtamonovDen <brightcolors46@gmail.com>
2020-11-14 16:58:12 +0300

    Update bnc

--- scripts/bnc.py
+++ scripts/bnc.py
@@ -37,17 +37,23 @@ def is_integer(x):
     '''
     Check if element is integer with Eps
     '''
-    return math.isclose(x, np.round(x),rel_tol=EPS)
+    return math.isclose(x, round(x), rel_tol=EPS)
 
 
 def is_int_list(l):
     '''
         Check if each elemnt in list is integer with Eps
     '''
-    return all(map(is_integer,l))
+    return all(map(is_integer, l))
 
 
-def separation(G, sol, model):
+def compare_with(x):
+    '''
+       Return nearest int if x is int. Prepare to compare with integers
+    '''
+    return round(x) if is_integer(x) else x
+
+def separation(G, sol, model, constraint_num=5):
     '''
       Find the most violated constraint
       by creating Maximum Independent set using coloring.
@@ -55,6 +61,7 @@ def separation(G, sol, model):
       Return found constraint of None if |S| <= 1
     '''
     logging.debug('Start Separation')
+    Cs = []  # Constraints list
     var = model.get_var_by_name   # get variable by name function
 
     # Apply coloring algorithm to graph
@@ -66,16 +73,20 @@ def separation(G, sol, model):
         nodes_by_colors[color].append(node)
         weights_by_color[color] += sol.get_value(var(f'x_{node}'))
 
-    max_weighted_color = max(weights_by_color, key=weights_by_color.get)
-    max_independent_set = nodes_by_colors[max_weighted_color]
-    C = model.le_constraint(sum(var(f'x_{i}') for i in max_independent_set), 1)  # TODO return several max constraints? 
+    max_weighted_colors = sorted(weights_by_color, key=weights_by_color.get)[-constraint_num:]  # Get constraint_num of max weighted sets
+    for color in reversed(max_weighted_colors):
+        independent_set = nodes_by_colors[color]
+        if compare_with(weights_by_color[color]) > 1:
+            C = model.le_constraint(sum(var(f'x_{i}') for i in independent_set), 1)  # TODO return several max constraints? 
+            Cs.append(C)
+            logging.debug(f'Found Max violated constraint: {str(C)}. Weight is {weights_by_color[color]}')
+        else:
+            C = model.le_constraint(sum(var(f'x_{i}') for i in independent_set), 1)
+            logging.debug(f'Found constraint {str(C)} has weight {weights_by_color[color]} which is less or equal to 1. Stop separation...')
+            break
 
-    if len(max_independent_set) <= 1:  # Not really sure about that TODO maybe just weight?
-        logging.debug(f'Found constraint {str(C)} is not enough. Stop separation...')
-        return None
-
-    logging.debug(f'Found Max violated constraint: {str(C)}')
-    return C
+    logging.debug('Seration finished')
+    return Cs
 
 
 def branching(vars_list, strategy='max'):
@@ -84,33 +95,33 @@ def branching(vars_list, strategy='max'):
         The strategy to chose:
           - random select index among not integers vars
           - select max x
+        If all vars are integer return -1
     '''
     non_int_vars = {index: x for index, x in enumerate(vars_list) if not is_integer(x)}
     if not non_int_vars:
-        raise Exception('All vars are integers')
-
+        return -1
     if strategy == 'max':
         return max(non_int_vars, key=non_int_vars.get)
     if strategy == 'random':
         return random.choice([*non_int_vars])
 
 
-def bnc_branch_left(model, branching_var, x):
+def bnc_branch_left(model, G, branching_var, x):
     left_int_constraint = model.add_constraint(
             branching_var <= math.floor(x)
     )
     logging.debug(f'Added LEFT constraint: {left_int_constraint}')
-    bnc(model)
+    bnc(model, G)
     model.remove_constraint(left_int_constraint)
     logging.debug(f'Remove LEFT constraint: {left_int_constraint}')
 
 
-def bnc_branch_right(model, branching_var, x):
+def bnc_branch_right(model, G, branching_var, x):
     right_int_constraint = model.add_constraint(
             branching_var >= math.ceil(x)
     )
     logging.debug(f'Added RIGHT constraint: {right_int_constraint}')
-    bnc(model)
+    bnc(model, G)
     model.remove_constraint(right_int_constraint)
     logging.debug(f'Remove RIGHT constraint: {right_int_constraint}')
 
@@ -136,39 +147,39 @@ def bnc(model, G):
     '''
         Main Branch-and-Cut function Called recurcively
     '''
+    local_sol = model.solve()
+
     global best_sol
     global best_x
-
-    local_sol = model.solve()
 
     if not local_sol:  # No solution found
         logging.debug('No solution found. Skip branch')
         return
 
     x = local_sol.get_all_values()
-    ub = local_sol.get_objective_value()
-    logging.debug(f'Found solution {ub}, {x}')
+    obj = local_sol.get_objective_value()
+    logging.debug(f'Found solution {obj}, {x}')
 
-    if not is_best_solution(ub):
+    if not is_best_solution(obj):
         logging.debug('Solution is not better. Skip branch')
         return
 
     tail_off_counter = 0
     last_obj = 0
+    logging.debug('Start Separation loop')
     while True:
 
         if tail_off_counter > TAIL_OFF_BOUND:
             logging.debug('TAIL_OFF_BOUND is reached. Break loop')
             break
 
-        logging.debug('Start Separation loop')
-        C = separation(G, local_sol, model)
+        Cs = separation(G, local_sol, model, constraint_num=100)
 
-        if not C:
-            logging.debug('Separation loop:: no found constraint after separation. Break loop')
+        if not Cs:
+            logging.debug('Separation loop: no found constraint after separation. Break loop')
             break
 
-        model.add_constraint(C)  # TODO add C constraint(s?) to model
+        model.add_constraints(Cs)
         local_sol = model.solve()
 
         if not local_sol:
@@ -191,8 +202,9 @@ def bnc(model, G):
             tail_off_counter += 1
             logging.debug(f'Objective function change is small. Attempt is increased to {tail_off_counter}')
 
-    if (is_int_list(x)):
-        possible_clique = np.where(np.array(x) > 0.5)[0] + 1  # from index 0..n-1 to nodes 1..n
+    i = branching(x)
+    if i < 0:
+        possible_clique = list(np.where(np.array(x) > 0.5)[0] + 1)  # from index 0..n-1 to nodes 1..n
         logging.debug(f'Solution is integer. Possible clique is {possible_clique}')
 
         broken_constraint = check_solution(possible_clique, G)
@@ -203,24 +215,26 @@ def bnc(model, G):
             return
 
         else:
+            var = model.get_var_by_name
             c = model.le_constraint(
-                f'x_{broken_constraint[0]}' + f'x_{broken_constraint[1]}', 1
+                var(f'x_{broken_constraint[0]}') + var(f'x_{broken_constraint[1]}'), 1
                 )
             logging.debug(f'It is not a real clique. Add new constraint: {c}')
             model.add_constraint(c)
             bnc(model, G)
+            logging.debug('Up back')
+        return
 
-    i = branching(x)
     branching_var = model.get_var_by_index(i)  # TODO get by name? x_i+1 ?
     logging.debug(f'Branching var is {branching_var}. Branching by value {x[i]}')
 
     if round(x[i]):
         # choose left branch if x[0] is closer to 0 and right one otherwise
-        bnc_branch_right(model, branching_var, x[i])
-        bnc_branch_left(model, branching_var, x[i])
+        bnc_branch_right(model, G, branching_var, x[i])
+        bnc_branch_left(model, G, branching_var, x[i])
     else:
-        bnc_branch_left(model, branching_var, x[i])
-        bnc_branch_right(model, branching_var, x[i])
+        bnc_branch_left(model, G, branching_var, x[i])
+        bnc_branch_right(model, G, branching_var, x[i])
 
 
 def check_solution(C, G):
@@ -229,12 +243,12 @@ def check_solution(C, G):
       and None if it is
     '''
     _C = C.copy()
-    is_clique = True
-    while _C and is_clique:
+    _is_clique = True
+    while _C and _is_clique:
         v = _C.pop()
-        is_clique = all(elem in G.neighbors(v) for elem in _C)
+        _is_clique = all(elem in G.neighbors(v) for elem in _C)
 
-    if not is_clique:
+    if not _is_clique:
         for elem in _C:
             if elem not in G.neighbors(v):
                 return (v, elem)
@@ -289,6 +303,29 @@ def clique_heuristic(G):
     return C_best
 
 
+def apply_coloring_constraints(model, G, painting_steps=10):
+    '''
+       Apply coloring to graph several times and add constraints to model.
+       Constraints: if nodes have same colors they can not be included in the same clique
+    '''
+    logging.info(f'Applying Coloring constraints {painting_steps} times')
+    for step in range(painting_steps):
+
+        var = model.get_var_by_name  # get variable by name function
+        # Apply coloring algorithm to graph
+        colors_by_nodes = nx.algorithms.coloring.greedy_color(G, 'random_sequential')
+
+        nodes_by_colors = defaultdict(list)
+        for node, color in colors_by_nodes.items():
+            nodes_by_colors[color].append(node)
+
+        # Add painting constarints to the model
+        for color, nodes_list in nodes_by_colors.items():
+            model.add_constraint(
+                model.le_constraint(sum(var(f'x_{i}') for i in nodes_list), 1)
+            )
+
+
 def init_graph(problem):
     '''
       Read graph from file
@@ -337,12 +374,13 @@ def main(args):
 
     problem_file = args.problem  # 'cliques_problems/san200_0.7_1.clq'
     problem_name = problem_file.split('/')[1]
-    init_logger(args.log_lvl, f'reports/{problem_name}.report')
+    init_logger(args.log_lvl, f'reports/bnc/{problem_name}.report')
 
     logging.info(f'Problem: {problem_name}')
 
     G = init_graph(problem_file)
     model = init_model(problem_name, G)
+    apply_coloring_constraints(model, G, painting_steps=args.color_step)
 
     C = clique_heuristic(G)
     init_solution(C, G)
@@ -355,7 +393,6 @@ def main(args):
     end = time.time()
     logging.info('Branch and Cuts have finished')
     logging.info(f'BnC time is {(end-start)/60} minutes')
-
     best_clique = [i for i, j in enumerate(best_x) if j > 0]
     logging.info(f'Best solution is {best_sol}: {best_clique}')
 
@@ -364,8 +401,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='BnC Solver for Maximum Clique Problem')
     parser.add_argument('-p', '--problem', dest="problem")
+    parser.add_argument('-l', '--log-lvl', dest="log_lvl", default='INFO')
     parser.add_argument('-c', '--color-step', dest="color_step", default=10, type=int)
-    parser.add_argument('-l', '--log-lvl', dest="log_lvl", default='info')
-
     args = parser.parse_args()
     main(args)
